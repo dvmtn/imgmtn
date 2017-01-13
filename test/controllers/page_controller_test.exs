9b8c3763c8fb790f3749c7c76fe06d33df635149
Adam Whittingham <adam@devmountain.co.uk>
2017-01-13 10:05:02 +0000

    Fix broken test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Imgmtn.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome anonymous"
   end
 end
